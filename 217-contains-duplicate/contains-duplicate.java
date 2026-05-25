0cb4837c275a47fb30ce296213339fc2230e28c3
SunidhiT123 <tiwarisunidhi924@gmail.com>
2026-05-25 16:14:34 +0530

    Time: 14 ms (88.78%) | Memory: 93.4 MB (59.17%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> s1=new HashSet<>();
+        for(int num: nums){
+            if(s1.contains(num)){
+                return true;
+            }
+            s1.add(num);
+        }
+        return false;
+    }
+}
